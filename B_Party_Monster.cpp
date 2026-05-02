0d37a0df98fb588f94f29687a7062e7c051878aa
Tanvir-136 <tanvir12islam34@gmail.com>
2026-05-02 21:21:56 +0600

    Add solution for B_Party_Monster problem

---
+++ B_Party_Monster.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout<<#x<<" = "<<x<<'\n';
+#define all(x) (x).begin(), (x).end()
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define ll long long
+#define MOD 1e9 + 7
+#define nl '\n'
+/*---------------------------------------------------------------*/
+const int N = 1e5 + 10;
+using namespace std;
+/*---------------------------------------------------------------*/
+
+//Idea is simple:: just count the number then check if the number of opening bracket is equal 
+// to the number of closing bracket
+
+void solve(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int cnt1 = count(all(s), '(');
+    if(cnt1 == n - cnt1){
+        yes
+    }else{
+        no
+    }
+
+}
+int main(){
+    FAST_IO;
+    //Start Here
+    int t=1;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
